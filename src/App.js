fbc327c64b65b6d62a866e4f57b8aa0feda8ba8f
severo <oliveira.severo@gmail.com>
2023-01-20 20:50:35 -0300

    feat: :rocket: Exemplo básico de uso criando um cubo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./Reset.css"
 import './App.css';
 
-function App() {
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
+const Scene = () => {
+  const scene = useRef();
+  useFrame(() => {
+    scene.current.rotation.y += 0.04;
+    scene.current.rotation.x += 0.04;
+    scene.current.rotation.z += 0.04;
+  });
   return (
-    <p> Olá Mundo!</p>
+    <group ref={scene}>
+      <Box>
+        <meshLambertMaterial color="white" />
+      </Box>
+    </group>
+  );
+};
+
+export default function App() {
+  return (
+    <Canvas>
+      <directionalLight intensity={0.5} />
+      <Scene />
+    </Canvas>
   );
 }
-
-export default App;
