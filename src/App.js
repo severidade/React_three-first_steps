79c909145b09e54c80f22a83fc94be848f4f899a
severo <oliveira.severo@gmail.com>
2023-01-20 20:56:28 -0300

    feat: :rocket: Usando texto dentro do canvas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 import BoxThree from "./componentes/BoxThree";
 
 export default function App() {
@@ -11,6 +11,15 @@ export default function App() {
     <Canvas>
       <directionalLight intensity={0.5} />
       <BoxThree />
+      <Text
+        fontSize={1}
+        position={[-1 , 2, 0]}
+        anchorX="center"
+        anchorY="middle"
+        textAlign="center"
+      >
+        Todas as flores
+      </Text>
     </Canvas>
   );
 }
