03c7171e28957d347a88e0d57aee8f760e394f58
severo <oliveira.severo@gmail.com>
2023-01-21 00:04:59 -0300

    feat: :rocket: efeitos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import React, { Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Text, Image } from "@react-three/drei";
+import { EffectComposer, Scanline, Noise, Bloom, Glitch, DotScreen } from "@react-three/postprocessing";
+
+import { BlendFunction } from 'postprocessing'
+import { BlurPass, Resizer, KernelSize } from 'postprocessing'
+import { GlitchMode } from 'postprocessing'
+
+
 import BoxThree from "./componentes/BoxThree";
 
 import img01 from "./img/1.png"
@@ -134,6 +141,41 @@ export default function App() {
           NEXT
         </Text>
       </Suspense>
+      <EffectComposer>
+        {/* <Scanline
+          blendFunction={BlendFunction.DARKEN} // blend mode
+          density={0.8} // scanline density
+          opacity={0.1}
+        /> */}
+        <Noise 
+          premultiply 
+          blendFunction={BlendFunction.ADD}
+          opacity={2}
+        />
+        {/* <Bloom
+          intensity={2} // The bloom intensity.
+          blurPass={undefined} // A blur pass.
+          width={Resizer.AUTO_SIZE} // render width
+          height={Resizer.AUTO_SIZE} // render height
+          kernelSize={KernelSize.LARGE} // blur kernel size
+          luminanceThreshold={0.5} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.3} // smoothness of the luminance threshold. Range is [0, 1]
+        /> */}
+        <Glitch
+          delay={[1.5, 3.5]} // min and max glitch delay
+          duration={[0.6, 1.0]} // min and max glitch duration
+          strength={[0.3, 1.0]} // min and max glitch strength
+          mode={GlitchMode.SPORADIC} // glitch mode
+          active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
+          ratio={1} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+          dtSize={1}
+        />
+        {/* <DotScreen
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          angle={Math.PI * 0.5} // angle of the dot pattern
+          scale={6} // scale of the dot pattern
+        /> */}
+      </EffectComposer>
     </Canvas>
   );
 }
